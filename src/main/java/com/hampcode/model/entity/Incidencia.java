a824f925c2a6fa183204299311d2b3ff8f958a10
LuisDextre99 <Luis Angel@DESKTOP-STV49GH>
2019-09-18 20:36:32 -0500

    Se agragagó la entidad Incidencia, el repositorio de la incidencia, el incidenciaBusiness, y IncidenciaController

---
+++ src/main/java/com/hampcode/model/entity/Incidencia.java
@@ -0,0 +1,49 @@
+package com.hampcode.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="incidencias")
+public class Incidencia {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private Supplier supplier; 
+	private String categoria;
+	private String fecha; 
+	private String comentario;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Supplier getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+}
