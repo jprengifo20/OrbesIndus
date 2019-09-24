5403dc03f6fba425d22b5049b1770279589cb178
LuisDextre99 <luisdexsmr@gmail.com>
2019-09-23 20:33:44 -0500

    Se agregó la clase IncidenciaController con el metodo getAllIncidencias

--- src/main/java/com/hampcode/controller/IncidenciaController.java
+++ src/main/java/com/hampcode/controller/IncidenciaController.java
@@ -1,9 +1,20 @@
 package com.hampcode.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.hampcode.business.IncidenciaBusiness;
+import com.hampcode.business.ProveedorBusiness;
+import com.hampcode.model.entity.Incidencia;
+import com.hampcode.model.entity.Product;
+import com.hampcode.model.entity.Proveedor;
+import com.hampcode.util.Message;
 
 @Named
 @ViewScoped
@@ -11,7 +22,59 @@ public class IncidenciaController implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
+	@Inject 
+	private IncidenciaBusiness incidenciaBusiness;
+	
+	@Inject 
+	private ProveedorBusiness proveedorBusiness; 
+	
+	private Incidencia incidencia;
+	private List<Incidencia> incidencias; 
+	private Incidencia incidenciaSeleccionada; 
+	
+	private Proveedor proveedor; 
+	private List<Proveedor>proveedores; 
+	
+	@PostConstruct
+	public void init()
+	{
+		incidencia= new Incidencia(); 
+		incidencias=new ArrayList<Incidencia>(); 
+		
+		proveedor= new Proveedor(); 
+		proveedores= new ArrayList<Proveedor>(); 
+	}
+	
+	public void getAllIncidencias()
+	{
+		try
+		{
+			incidencias= incidenciaBusiness.getall();
+		}catch(Exception e)
+		{
+			Message.messageError("Error en la carga de incidencias");
+		}
+	}
+	
+	/*public String newIncidencias()
+	{
+		try {
+			this.incidencias=incidenciaBusiness.getall();
+			resetForm();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}*/
+	
 
 	
-	int numerox;
+	
+	
+	
+	
+	
+	
+	
+	
 }
