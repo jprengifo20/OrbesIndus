c5447919e416b57ff8f81949f628a0d438873e7d
LuisDextre99 <luisdexsmr@gmail.com>
2019-09-23 20:36:00 -0500

    Se agregó la clase IncidenciaBusiness con los métodos(actualizados) insert, update, getAll

--- src/main/java/com/hampcode/business/IncidenciaBusiness.java
+++ src/main/java/com/hampcode/business/IncidenciaBusiness.java
@@ -1,6 +1,32 @@
 package com.hampcode.business;
 
-public class IncidenciaBusiness {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import com.hampcode.model.entity.Incidencia;
+import com.hampcode.model.repository.IncidenciaRepository;
+
+@Named
+public class IncidenciaBusiness implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private IncidenciaRepository incidenciaRepository; 
+	@Transactional
+	public long insert (Incidencia incidencia) throws Exception
+	{
+		return incidenciaRepository.insert(incidencia); 
+	}
+	
+	public long update(Incidencia incidencia)throws Exception
+	{
+		return incidenciaRepository.update(incidencia); 
+	}
+	public List<Incidencia> getall()throws Exception
+	{
+		return incidenciaRepository.findAll();
+	}
 	
 	
 }
